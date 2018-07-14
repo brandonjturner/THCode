7607b613c573fefb06cef967b5cf8e56a6718635
Brandon Turner <brandon88@gmail.com>
2018-07-14 11:42:44 -0700

    working table

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 $("#add-strain-btn").on("click", function(event) {
   event.preventDefault();
   var strainName = $("#strain-name-input").val().trim();
-var queryURL = "https://cors-anywhere.herokuapp.com/https://www.cannabisreports.com/api/v1.0/strains/search/" + strainName;
-$.ajax({
-  url: queryURL,
-  method: "GET",
-  headers: {
-},
-}).then(function(response) {
-  console.log(response);
+  var queryURL = "https://cors-anywhere.herokuapp.com/https://www.cannabisreports.com/api/v1.0/strains/search/" + strainName;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    headers: {
+    },
+  }).then(function(response) {
+    console.log(response);
+    addNewRow(response.data, strainName, 1, 2, 3);
+  });
 });
-});
-var newRow = $("<tr>").append(
-  $("<td>").text(response[0].lineage.name);
-  $("<td>").text(response[0].genetics);
-  $("<td>").text(response[0].genetics.image));
-$("#strain-table > tbody").append(newRow)
+
+function addNewRow (data, strainName, moreargs, anotherone, thiojwefoij) {
+  console.log(data[0]);
+  var img = data[0].image;
+  var newRow = $("<tr>").append(
+    $("<td>").text(strainName),
+    $("<td>").text(data[0].genetics.names),
+    $("<td>").append($('<img>').attr('src', img))
+  );
+  $("#strain-table > tbody").append(newRow);
+}
