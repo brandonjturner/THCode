a47e0027d8316f7cc6d42f2363fa91ac1a60f404
Brandon Turner <b_turner@maxonoffice.com>
2018-07-17 12:40:49 -0700

    map feature added

--- app.js
+++ app.js
@@ -20,3 +20,36 @@ function addNewRow (data, strainName) {
   );
   $("#strain-table > tbody").prepend(newRow);
 }
+var map;
+var infowindow;
+function initMap() {
+  var losAngeles = {lat: 34.063145, lng: -118.436755};
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: losAngeles,
+    zoom: 15
+  });
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: losAngeles,
+    radius: 10000,
+    keyword: 'collective'
+  }, callback);
+}
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
